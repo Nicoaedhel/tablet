474a73d8f9f99991b43bdf43c05997d993b782bb
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-07 14:23:26 +0200

    add commentary

--- assets/app.js
+++ assets/app.js
@@ -9,14 +9,21 @@ function change(){
   $.ajax({
     url: "layers/",
     success: function(data){
-
+      //find files in folder
        $(data).find("a").each(function(){
           files.push($(this).attr("href"));
        });
        console.log(files);
+
        var d = new Date();
        var n = d.getTime();
+
+       // check time interval
+
+       // Display result
        $('.board_target').css('background','url("layers/'+files[0]+'"')
+
+       //reload
        setTimeout(function () {
          files = [];
          change()

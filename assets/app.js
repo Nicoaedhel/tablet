5f88007f5e8ccd89cae66efe87652c875cb0930a
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-07 13:49:28 +0200

    Edit board_target

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,11 @@
 setTimeout(function () {
   $('.board').fadeIn(500);
 }, 200);
+
+setTimeout(function () {
+  $('.board_target').css('background','url("layers/07-10-2016_15h45:16h00_heure3.png"')
+}, 1000);
+
+setTimeout(function () {
+  $('.board_target').css('background','url("layers/07-10-2016_14h45:15h00_heure2.png"')
+}, 2000);

a161b23767eb09873569da81aa2990fbb568cb71
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-07 18:44:49 +0200

    Add time algo

--- assets/app.js
+++ assets/app.js
@@ -6,29 +6,23 @@ setTimeout(function () {
 var files = [];
 
 function change(){
-  $.ajax({
-    url: "layers/",
-    success: function(data){
-      //find files in folder
-       $(data).find("a").each(function(){
-          files.push($(this).attr("href"));
-       });
-       console.log(files);
-
-       var d = new Date();
-       var n = d.getTime();
-
-       // check time interval
-
-       // Display result
-       $('.board_target').css('background','url("layers/'+files[0]+'"')
-
-       //reload
-       setTimeout(function () {
-         files = [];
-         change()
-       }, 1000);
+  $.getJSON("layers/config.json", function(data) {
+    var today = new Date();;
+    var h = today.getHours();
+    var m = today.getMinutes();
+    i = 0;
+    for (var prop in data.layers) {
+      time = data.layers[i][1].split("/");
+      time_1 = time[0];
+      time_2 = time[1];
+      if (time_1 <= ""+h+":"+""+m+"")
+          if (time_2 >= ""+h+":"+""+m+"")
+            $('.board_target').css('background','url("layers/'+data.layers[i][0]+'.png"')
+      i++;
     }
+    setTimeout(function () {
+      change()
+    }, 1000);
   });
 }
 

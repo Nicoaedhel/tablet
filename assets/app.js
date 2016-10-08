882faefd8e32cc2fcd51b97a93d2ed0301924859
Nicoaedhel <monsieurhiboux@imac-de-monsieur.home>
2016-10-08 17:50:04 +0200

    Add date interval JJ/MM/AAAA

--- assets/app.js
+++ assets/app.js
@@ -10,18 +10,38 @@ function change(){
     var today = new Date();;
     var h = today.getHours();
     var m = today.getMinutes();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    }
+    if(mm<10){
+        mm='0'+mm
+    }
+    var today_time = dd+'/'+mm+'/'+yyyy;
     i = 0;
     for (var prop in data.layers) {
-      time = data.layers[i][1].split("/");
+      time = data.layers[i][2].split("/");
       time_1 = time[0];
       time_2 = time[1];
-      if (time_1 <= ""+h+":"+""+m+"")
-          if (time_2 >= ""+h+":"+""+m+""){
-            $('.board_target').css('background','url("layers/'+data.layers[i][0]+'.png"')
-            break;
-          }
+      date_time = data.layers[i][1].split(":");
+      date_time_1 = date_time[0];
+      date_time_2 = date_time[1];
+      if(date_time_1 <= today_time)
+        if(date_time_2 >= today_time)
+          if (time_1 <= ""+h+":"+""+m+"")
+              if (time_2 >= ""+h+":"+""+m+""){
+                $('.board_target').css('background','url("layers/'+data.layers[i][0]+'.png"')
+                break;
+              }
+              else
+                $('.board_target').css('background','')
           else
             $('.board_target').css('background','')
+        else
+          $('.board_target').css('background','')
       else
         $('.board_target').css('background','')
       i++;
